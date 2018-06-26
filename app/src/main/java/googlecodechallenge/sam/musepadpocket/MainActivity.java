7a9ae8340939a1ad38701efe2678b10b77424188
live-beta <wanjala.sam.s@gmail.com>
2018-06-27 01:00:43 +0300

    Initial Commit

---
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/MainActivity.java
@@ -0,0 +1,13 @@
+package googlecodechallenge.sam.musepadpocket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
