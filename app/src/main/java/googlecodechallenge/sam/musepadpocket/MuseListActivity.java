8900f6c0361718342a7e7895b004dbfc7b4c076b
live-beta <wanjala.sam.s@gmail.com>
2018-06-27 04:50:14 +0300

    (app-design): add initial app design
    - Imports assets
    - creates layouts
    - adds widgets and nested views to layouts
    
    [Implemnets #Task-app-design]

---
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/MuseListActivity.java
@@ -0,0 +1,18 @@
+package googlecodechallenge.sam.musepadpocket;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sam on 6/27/18.
+ */
+
+public class MuseListActivity extends AppCompatActivity {
+
+@Override
+    protected void onCreate(Bundle onSavedInstanceState){
+    super.onCreate(onSavedInstanceState);
+    setContentView(R.layout.muse_list_layout);
+
+}
+}
