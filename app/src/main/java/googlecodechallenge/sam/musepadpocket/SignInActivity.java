862a84fd85644703c34f5fd3b53c10eb0610a238
live-beta <wanjala.sam.s@gmail.com>
2018-06-30 17:33:24 +0300

    (refactor): refactor activity name
    - Refactor Main activity name to Sign in activity
    [Implements #Task-base-project-best-practices]

--- app/src/main/java/googlecodechallenge/sam/musepadpocket/SignInActivity.java
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/SignInActivity.java
@@ -3,7 +3,7 @@ package googlecodechallenge.sam.musepadpocket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
