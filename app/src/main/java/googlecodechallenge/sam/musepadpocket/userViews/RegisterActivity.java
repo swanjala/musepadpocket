69382d26bd5c565b831a6be61ed575ae6dadfc86
Sammy Wanjala <sam@Sam.local>
2019-01-14 15:33:39 +0300

    (Refactor) Implement butterknife to registeration activity

--- app/src/main/java/googlecodechallenge/sam/musepadpocket/userViews/RegisterActivity.java
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/userViews/RegisterActivity.java
@@ -2,6 +2,7 @@ package googlecodechallenge.sam.musepadpocket.userViews;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +12,9 @@ import android.widget.Toast;
 
 import java.net.URL;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import googlecodechallenge.sam.musepadpocket.R;
 import googlecodechallenge.sam.musepadpocket.networkutils.ApiCalls;
 import googlecodechallenge.sam.musepadpocket.networkutils.BuildUrls;
@@ -21,22 +25,30 @@ import googlecodechallenge.sam.musepadpocket.networkutils.BuildUrls;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
+    @BindView(R.id.bt_register)
     Button bt_register;
-    EditText et_user_name, et_register_password, et_register_email;
-    private String username, email, password;
+    @BindView(R.id.et_name)
+    EditText et_user_name;
+    @BindView(R.id.et_password)
+    EditText et_register_password;
+    @BindView(R.id.et_email)
+    EditText et_register_email;
+
+    @NonNull
+    private String
+            username = "",
+            email = "",
+            password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        bt_register = findViewById(R.id.bt_register);
-        et_user_name= findViewById(R.id.et_name);
-        et_register_email = findViewById(R.id.et_email);
-        et_register_password = findViewById(R.id.et_password);
-        bt_register.setOnClickListener(this);
+        ButterKnife.bind(this);
+      //  bt_register.setOnClickListener(this);
 
     }
-    @Override
+    @OnClick(R.id.bt_register)
     public void onClick(View v){
         int viewId = v.getId();
         if(viewId ==R.id.bt_register){
@@ -48,10 +60,10 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
+
     public void signUp(){
         BuildUrls buildUrls = new BuildUrls(this);
         URL url = buildUrls.buildUrlForUserSignUp();
-        Log.d("Urlm",String.valueOf(url));
 
         ApiCalls apiCalls = new ApiCalls(url,username,password,email);
         boolean successRegister = apiCalls.registerUser();
