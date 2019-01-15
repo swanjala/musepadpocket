0454073718903e1d46926c9603b8b929dfb37d99
swanjala <wanjala.sam.s@gmail.com>
2019-01-15 17:26:00 +0300

    (feature): add retrofit network instance
    - Pull in the auth token from shared preferences
    - Initialize Gson setLenient
    - Create an interceptor for token injection
    - Create and return Retrofit Instance

---
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/networkutils/RetrofitInstance.java
@@ -0,0 +1,56 @@
+package googlecodechallenge.sam.musepadpocket.networkutils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+
+import googlecodechallenge.sam.musepadpocket.networkutils.INetwork;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+
+    public static INetwork retrofitInstance(String url, Context context){
+
+        final SharedPreferences sharedPreferences = PreferenceManager
+                .getDefaultSharedPreferences(context);
+
+        /* Gson handling for JSON*/
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        /*Token Interceptor for network call auth */
+        Interceptor interceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request modifiedRequest = chain.request().newBuilder()
+                        .addHeader("Authorization", "Bearer "
+                        + sharedPreferences.getString("token", ""))
+                        .build();
+                return chain.proceed(modifiedRequest);
+            }
+        };
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.interceptors().add(interceptor);
+        OkHttpClient client = builder.build();
+
+        Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client);
+        Retrofit retrofit = retrofitBuilder.build();
+
+        return retrofit.create(INetwork.class);
+    }
+}
