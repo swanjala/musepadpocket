62b4e850fc6626c0346930ce46aa4b25cefccd29
swanjala <wanjala.sam.s@gmail.com>
2019-01-15 16:33:26 +0300

    (ft): add data models
    - Add muse data model
    - Add muse item data model

---
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/models/MuseModel.java
@@ -0,0 +1,68 @@
+package googlecodechallenge.sam.musepadpocket.models;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MuseModel  {
+
+    @SerializedName("creator")
+    @Expose
+    private String creator;
+    @SerializedName("date_created")
+    @Expose
+    private String dateCreated;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("items")
+    @Expose
+    private List<Item> items = null;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<ItemModel> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+}
