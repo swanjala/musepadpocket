7e5c0d2f02208fe70be7735ff9cb8d0c14308b95
swanjala <wanjala.sam.s@gmail.com>
2019-01-19 22:52:37 +0300

    (refactor): refactor Apiresponse
    - Refactor login calls to ApiResponse

--- app/src/main/java/googlecodechallenge/sam/musepadpocket/api/ApiResponse.java
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/api/ApiResponse.java
@@ -1,33 +1,78 @@
 package googlecodechallenge.sam.musepadpocket.api;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import googlecodechallenge.sam.musepadpocket.models.MuseModel;
+import googlecodechallenge.sam.musepadpocket.models.UserModel;
+import googlecodechallenge.sam.musepadpocket.museViews.MuseListActivityFree;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ApiResponse {
 
-    private ArrayList<MuseModel> museListData;
-    private Throwable throwable;
+    public ArrayList<MuseModel> museListData;
+    public Throwable throwable;
 
-    ApiResponse(ArrayList<MuseModel> museListData,
-                Throwable throwable){
-        this.throwable = throwable;
-        this.museListData = museListData;
+    private Context context;
+    private String userName, password,url;
+
+    public ApiResponse(Context context, String url){
+        this.context = context;
+        this.url = url;
+    }
+    public ApiResponse(String userName,String password,Context context, String url){
+        this.userName = userName;
+        this.password = password;
+        this.url = url;
+        this.context = context;
     }
 
-    ApiResponse(ArrayList<MuseModel> museListData){
-        this.museListData = museListData;
-    }
+    public void login(){
 
-    ApiResponse(Throwable throwable){
-        this.throwable = throwable;
-    }
+        Call<UserModel> call = new ApiManager(userName,password,context, url)
+                .login();
 
-    @NonNull
-    public ArrayList<MuseModel> getMuseListData(){
-        return museListData;
+        call.enqueue(new Callback<UserModel>() {
+            @Override
+            public void onResponse(Call<UserModel> call, Response<UserModel> response) {
+
+
+                if (response.body().getToken() != null){
+
+                    Toast.makeText(context, "Token info" + response.body().getToken(),
+                            Toast.LENGTH_LONG).show();
+
+                    SharedPreferences sharedPreferences = PreferenceManager
+                            .getDefaultSharedPreferences(context);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("token", response.body().getToken());
+                    editor.apply();
+                    Intent intent = new Intent(context,MuseListActivityFree.class);
+                    context.startActivity(intent);
+                }
+                else{
+                    Toast.makeText(context, "Login not sucessful",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserModel> call, Throwable t) {
+
+                Log.d("Retrofit Error", t.getLocalizedMessage());
+
+            }
+        });
+
     }
     @NonNull
     public Throwable getThrowable() {
