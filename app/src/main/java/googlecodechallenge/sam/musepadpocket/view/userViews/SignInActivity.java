2d8ec073aab4f9386d6ef6f2bded2c10a6632d2a
swanjala <wanjala.sam.s@gmail.com>
2019-01-23 00:19:55 +0300

    (update):sign in activity - add alert dialogs

--- app/src/main/java/googlecodechallenge/sam/musepadpocket/view/userViews/SignInActivity.java
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/view/userViews/SignInActivity.java
@@ -1,28 +1,25 @@
-package googlecodechallenge.sam.musepadpocket.userViews;
+package googlecodechallenge.sam.musepadpocket.view.userViews;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.StrictMode;
-import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.net.URL;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import googlecodechallenge.sam.musepadpocket.R;
-import googlecodechallenge.sam.musepadpocket.api.ApiManager;
-import googlecodechallenge.sam.musepadpocket.api.ApiResponse;
-import googlecodechallenge.sam.musepadpocket.models.UserModel;
-import googlecodechallenge.sam.musepadpocket.museViews.MuseListActivityFree;
+import googlecodechallenge.sam.musepadpocket.datamodel.api.ApiCalls;
+import googlecodechallenge.sam.musepadpocket.datamodel.apiinterfaces.IApiCalls;
 
 /*
 * Class lets the user sign into the application
@@ -50,6 +47,12 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         ButterKnife.bind(this);
         signup.setOnClickListener(this);
 
+        if (savedInstanceState != null){
+
+            SharedPreferences preferences = getSharedPreferences("token",0);
+            preferences.edit().remove("token").commit();
+        }
+
         StrictMode.ThreadPolicy policy =new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
@@ -59,20 +62,56 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v){
         int viewId = v.getId();
         if (viewId ==R.id.bt_signup){
-
-            Intent intent = new Intent(this, RegisterActivity.class);
-            startActivity(intent);
+            showSighnUpDialog(this);
 
         }else if (viewId ==R.id.bt_login){
 
             this.userName = et_user_name_input.getText().toString();
             this.password = et_password_input.getText().toString();
-            ApiResponse apiResponse = new ApiResponse(userName, password,this, getResources()
+            IApiCalls apiCalls = new ApiCalls(userName, password,this, getResources()
                     .getString(R.string.muse_base_url));
 
-            apiResponse.login();
+            apiCalls.login();
 
         }
+    }
+
+    protected void showSighnUpDialog(final Context context) {
+        View promptView = LayoutInflater
+                .from(context)
+                .inflate(R.layout.signup_dialog, null);
+        final EditText et_userName, et_password, et_email;
+
+        et_userName = promptView.findViewById(R.id.et_signup_dialog_user_name_);
+        et_password = promptView.findViewById(R.id.et_signup_dialog_password);
+        et_email = promptView.findViewById(R.id.et_dialog_email);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+        alertDialogBuilder.setView(promptView);
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("Sign Me Up",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(context, "Signed up", Toast.LENGTH_LONG).show();
+                              IApiCalls apiCalls = new ApiCalls(et_userName.getText().toString(),
+                                      et_password.getText().toString(),et_email.getText().toString(),
+                                      getApplicationContext());
+                              apiCalls.register();
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+
     }
 
 
