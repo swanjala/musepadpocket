f48d1215700f123a2c70dca64e0f7c8aff364cf2
swanjala <wanjala.sam.s@gmail.com>
2019-01-23 00:14:30 +0300

    (update) user request model
    - add user request constructors for
    logins and registrations

--- app/src/main/java/googlecodechallenge/sam/musepadpocket/model/UserRequestModel.java
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/model/UserRequestModel.java
@@ -1,11 +1,16 @@
 package googlecodechallenge.sam.musepadpocket.model;
 
-public class LoginModel {
+public class UserRequestModel {
 
-    String username, password;
-    public LoginModel(String username, String password){
+    String username, password,email;
+    public UserRequestModel(String username, String password){
         this.username = username;
         this.password = password;
 
+    }
+    public UserRequestModel(String username,String password, String email){
+        this.username = username;
+        this.password = password;
+        this.email = email;
     }
 }
