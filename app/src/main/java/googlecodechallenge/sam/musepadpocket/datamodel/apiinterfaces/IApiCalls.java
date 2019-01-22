0c2b90297da5ed3fbf8cf5cf2a3eb481b916fe21
swanjala <wanjala.sam.s@gmail.com>
2019-01-23 00:00:33 +0300

    (add): api call interface

--- app/src/main/java/googlecodechallenge/sam/musepadpocket/datamodel/apiinterfaces/IApiCalls.java
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/datamodel/apiinterfaces/IApiCalls.java
@@ -1,7 +1,8 @@
-package googlecodechallenge.sam.musepadpocket.datamodel.api;
+package googlecodechallenge.sam.musepadpocket.datamodel.apiinterfaces;
 
 public interface IApiCalls {
 
     boolean getMuseViewModel();
     void login();
+    void register();
 }
