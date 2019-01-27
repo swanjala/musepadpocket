648264706d655fb5bbf00c39c3a2468ba2b70fb1
swanjala <wanjala.sam.s@gmail.com>
2019-01-27 23:12:17 +0300

    (refactor): add database access object for users

--- app/src/main/java/googlecodechallenge/sam/musepadpocket/datamodel/databaseinterfaces/UserDao.java
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/datamodel/databaseinterfaces/UserDao.java
@@ -1,4 +1,20 @@
 package googlecodechallenge.sam.musepadpocket.datamodel.databaseinterfaces;
 
-public class UserDao {
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+import googlecodechallenge.sam.musepadpocket.model.UserModel;
+
+@Dao
+public interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertuser(UserModel userModel);
+
+    @Query("SELECT * FROM users where userName = :userName")
+    List<UserModel> getUsers(String userName);
 }
