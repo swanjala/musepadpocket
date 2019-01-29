b3e5d3e1aafad97fb35e1b8a0b4dd1bc64d12ac9
swanjala <wanjala.sam.s@gmail.com>
2019-01-29 19:10:51 +0300

    (update): user request model - move email to constructor

--- app/src/main/java/googlecodechallenge/sam/musepadpocket/model/UserRequestModel.java
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/model/UserRequestModel.java
@@ -3,14 +3,10 @@ package googlecodechallenge.sam.musepadpocket.model;
 public class UserRequestModel {
 
     String username, password,email;
-    public UserRequestModel(String username, String password){
-        this.username = username;
-        this.password = password;
+    public UserRequestModel(UserModel userModel){
+        this.username = userModel.getUserName();
+        this.password = userModel.getPassword();
+        this.email = userModel.getEmail();
+    }
 
-    }
-    public UserRequestModel(String username,String password, String email){
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
 }
