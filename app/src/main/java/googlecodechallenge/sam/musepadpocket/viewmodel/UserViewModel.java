d9b443dfd5478923bf76de1851c5774d28250bda
swanjala <wanjala.sam.s@gmail.com>
2019-01-30 05:19:39 +0300

    (refactor): add user view model
    - Implement login user functionality
    - Implement signup functionality

--- app/src/main/java/googlecodechallenge/sam/musepadpocket/viewmodel/UserViewModel.java
+++ app/src/main/java/googlecodechallenge/sam/musepadpocket/viewmodel/UserViewModel.java
@@ -1,4 +1,37 @@
 package googlecodechallenge.sam.musepadpocket.viewmodel;
 
-public class UserViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+import googlecodechallenge.sam.musepadpocket.datamodel.api.ApiCallInstance;
+import googlecodechallenge.sam.musepadpocket.datamodel.api.ApiCalls;
+import googlecodechallenge.sam.musepadpocket.datamodel.apiinterfaces.IApiCalls;
+import googlecodechallenge.sam.musepadpocket.model.UserModel;
+
+public class UserViewModel extends AndroidViewModel {
+
+    private static final String TAG = UserViewModel.class.getSimpleName();
+
+    public UserViewModel(@NonNull Application application){
+        super(application);
+
+    }
+
+    public void loginUser(String userName,String password){
+
+        UserModel userModel = new UserModel(userName,password);
+
+        IApiCalls apiCalls = new ApiCalls(userModel,
+                getApplication().getApplicationContext());
+        apiCalls.login();
+    }
+
+    public void registerUser(String userName, String password,
+                             String email){
+        UserModel userModel = new UserModel(userName, password, email);
+        IApiCalls apiCalls = new ApiCalls(userModel,
+                getApplication().getApplicationContext());
+        apiCalls.register();
+    }
 }
